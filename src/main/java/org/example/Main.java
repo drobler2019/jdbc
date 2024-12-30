5965d6e412a44bfab7e3e8c0868f3d4781affa7c
drobler2019 <drobler2019@gmail.com>
2024-12-29 23:24:41 -0500

    CRUD completo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,45 @@
 package org.example;
 
 import org.example.configuration.ConnectionDataSource;
+import org.example.entities.Product;
+import org.example.repository.ProductRepository;
 import org.example.repository.impl.ProductRepositoryImpl;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         var productRepository = new ProductRepositoryImpl();
+        updateProduct(productRepository);
+        ConnectionDataSource.closeConnection();
+    }
+
+    public static void listProducts(ProductRepository productRepository) {
         productRepository.findAll().forEach(System.out::println);
+    }
+
+    public static void findProduct(ProductRepository productRepository) {
         var optionalProduct = productRepository.findById(32L);
         optionalProduct.ifPresentOrElse(System.out::println, () -> System.out.println("usuario no encontrado"));
-        ConnectionDataSource.closeConnection();
+    }
+
+    public static void saveProduct(ProductRepository productRepository) {
+        var newProduct = new Product(null, "Secadora", 3000.0, LocalDate.now());
+        productRepository.save(newProduct);
+    }
+
+    public static void updateProduct(ProductRepository productRepository) {
+        var optionalProduct = productRepository.findById(11L);
+        optionalProduct.ifPresentOrElse(product -> {
+            var newProduct = new Product(product.id(), "Juego de niñas", 3000.0, null);
+            productRepository.save(newProduct);
+        }, () -> System.out.println("usuario no encontrado"));
+    }
+
+    public static void deleteProduct(ProductRepository productRepository) {
+        productRepository.deleteById(12L);
     }
 
 }
--- src/main/java/org/example/repository/impl/ProductRepositoryImpl.java
+++ src/main/java/org/example/repository/impl/ProductRepositoryImpl.java
@@ -5,6 +5,7 @@ import org.example.entities.Product;
 import org.example.repository.ProductRepository;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -44,20 +45,49 @@ public class ProductRepositoryImpl implements ProductRepository {
                 return Optional.ofNullable(product);
             }
         } catch (SQLException e) {
-            final var message = String.format("code: %s - detail: %s", e.getSQLState(), e.getMessage());
-            System.err.println(message);
+            this.logError(e);
             return Optional.empty();
         }
     }
 
     @Override
     public void save(Product product) {
-
+        String sql;
+        if (this.hasId(product.id())) {
+            sql = "UPDATE productos SET nombre=?, precio=? WHERE id=?";
+        } else {
+            sql = "INSERT INTO productos(nombre,precio,fecha_registro) VALUES (?,?,?)";
+        }
+        try (var stmt = this.getConnection().prepareStatement(sql)) {
+            stmt.setString(1, product.nombre());
+            stmt.setDouble(2, product.precio());
+            if (this.hasId(product.id())) {
+                stmt.setLong(3, product.id());
+            } else {
+                stmt.setDate(3, Date.valueOf(product.fechaRegistro()));
+            }
+            stmt.executeUpdate();
+            final var message = sql.contains("UPDATE") ? String.format("producto %s %s con éxito!", product.id(), "actualizado")
+                    : "producto guardado con éxito!";
+            this.success(message);
+        } catch (SQLException e) {
+            this.logError(e);
+        }
     }
 
     @Override
     public void deleteById(Long id) {
+        try (var stmt = this.getConnection().prepareStatement("DELETE FROM productos WHERE id=?")) {
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+            this.success("producto " + id + " eliminado con éxito");
+        } catch (SQLException e) {
+            this.logError(e);
+        }
+    }
 
+    private boolean hasId(Long id) {
+        return id != null && id > 0;
     }
 
     private Product getProduct(ResultSet resultSet) throws SQLException {
@@ -66,5 +96,15 @@ public class ProductRepositoryImpl implements ProductRepository {
                 resultSet.getDouble("precio"),
                 resultSet.getDate("fecha_registro").toLocalDate());
     }
+
+    private void success(String message) {
+        System.out.println(message);
+    }
+
+    private void logError(SQLException e) {
+        final var message = String.format("code: %s - detail: %s", e.getSQLState(), e.getMessage());
+        System.err.println(message);
+    }
+
 
 }
